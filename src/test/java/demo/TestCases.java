dcf78c10d47643ce095d2f16280e29445daf520a
divatepratik10 <pratikrdivate3210@gmail.com>
2024-07-15 10:32:11 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,25 +11,100 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
+    private Wrappers wrappers;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow `testCase01` `testCase02`... format or what is provided in
+     * instructions
      */
 
-     
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
+    @Test
+    public void testCase01() throws InterruptedException {
+
+        System.out.println("START : testCase01 is Started" + "\n");
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+
+        wrappers.sendKeys(By.xpath("(//*[@class='whsOnd zHQkBf'])[1]"), "Crio Learner");
+        System.out.println("Pass : Name is Printed");
+
+        long epoch = System.currentTimeMillis() / 1000;
+
+        wrappers.sendKeys(By.xpath("//*[@class='KHxj8b tL9Q4c']"), "I want to be the best QA Engineer! " + epoch);
+        System.out.println("Pass : Why are you practicing Automation is Printed");
+        Thread.sleep(500);
+
+        String exp = "0 - 2"; // 3 - 5 / 6 - 10 / > 10
+        wrappers.click(By
+                .xpath("//*[text()='" + exp + "']/parent::div/parent::div//preceding-sibling::div/div[@role='radio']"));
+        System.out.println("Pass : Experience is selected");
+
+        String skill_1 = "Java", skill_2 = "Selenium", skill_3 = "Springboot", skill_4 = "TestNG";
+
+        wrappers.click(By.xpath(
+                "//*[text()='" + skill_1 + "']/parent::div/parent::div//preceding-sibling::div[@role='checkbox']"));
+        Thread.sleep(500);
+        wrappers.click(By.xpath(
+                "//*[text()='" + skill_2 + "']/parent::div/parent::div//preceding-sibling::div[@role='checkbox']"));
+        Thread.sleep(500);
+        wrappers.click(By.xpath(
+                "//*[text()='" + skill_4 + "']/parent::div/parent::div//preceding-sibling::div[@role='checkbox']"));
+        Thread.sleep(500);
+        System.out.println("Pass : Skills are selected");
+
+        String prefix = "Mr"; // Ms / Mrs / Dr / Rather not say
+        wrappers.click(By.xpath("//*[@role='option']"));
+        Thread.sleep(500);
+        wrappers.click(By.xpath("//*[@data-value='" + prefix + "' and @role='option']"));
+        System.out.println("Pass : Prefix is selected");
+        Thread.sleep(500);
+
+        wrappers.sendKeys(By.xpath("//*[@type='date']"), wrappers.date7DaysAgo());
+        System.out.println("Pass : 7 days earlier date is printed");
+        Thread.sleep(500);
+
+        String time = wrappers.currentTime();
+        char[] arr = time.toCharArray();
+        StringBuilder hr = new StringBuilder();
+        StringBuilder mn = new StringBuilder();
+        hr.append(arr[0]);
+        hr.append(arr[1]);
+        mn.append(arr[3]);
+        mn.append(arr[4]);
+        String hrs = hr.toString();
+        String mns = mn.toString();
+
+        wrappers.sendKeys(By.xpath("//*[@aria-label='Hour']"), hrs);
+        Thread.sleep(500);
+        wrappers.sendKeys(By.xpath("//*[@aria-label='Minute']"), mns);
+        System.out.println("Pass : current time is printed");
+        Thread.sleep(500);
+
+        wrappers.click(By.xpath("//*[@role='button' and @jsname='M2UYVd']"));
+        System.out.println("Pass : Clicked on submit button");
+        Thread.sleep(1000);
+
+        System.out.println("Pass : Form submitted Successfully. Success message printed : "
+                + wrappers.getText(By.xpath("//*[@class='vHW8K']")));
+        System.out.println("Pass : Form submitted Successfully and success message printed" + "\n");
+        
+        System.out.println("END : testCase01 is Ended");
+
+    }
+
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -42,18 +118,21 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+
+        driver.get(
+                "https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform?pli=1");
+
+        wrappers = new Wrappers(driver, 10);
     }
 
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
-
     }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,9 +3,12 @@ package demo.wrappers;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,7 +16,69 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+    String currntTime = "";
+
+    public Wrappers(WebDriver driver, int timee) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(timee));
+    }
+
+    public void click(By locator) {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        element.click();
+    }
+
+    public void sendKeys(By locator, String text) {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public String getText(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).getText();
+    }
+
+    public String date7DaysAgo() {
+        LocalDateTime sevenDaysAgo = LocalDateTime.now().minus(7, ChronoUnit.DAYS);
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a");
+        String dateTime = sevenDaysAgo.format(dateFormatter);
+        String[] arr = dateTime.trim().split("\\s+");
+        return arr[0];
+    }
+
+    public String currentTime() {
+        LocalTime currentTime = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss a");
+        String formattedTime = currentTime.format(formatter);
+        currntTime = formattedTime;
+        String[] arrT = formattedTime.trim().split("\\s+");
+        char[] arrC = arrT[0].toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<=4; i++) {
+            sb.append(arrC[i]);
+        }
+        return sb.toString();
+    }
+
+    public String amORpm() {
+        char[] chArr = currntTime.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(int i=chArr.length-2; i<chArr.length; i++) {
+            sb.append(chArr[i]);
+        }
+        return sb.toString();
+    }
+
+    public boolean isElementPresent(By locator) {
+        try {
+            wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
